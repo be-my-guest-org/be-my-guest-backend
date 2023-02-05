e5ed0d94eb43ebad76aceabced61077a0ac04159
JordanGottardo <jordangottardo@gmail.com>
2023-02-05 17:56:24 +0100

    add API gateway

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
 import { Construct } from "constructs";
@@ -11,6 +12,10 @@ export class CdkWorkshopStack extends Stack {
             runtime: lambda.Runtime.NODEJS_14_X,
             code: lambda.Code.fromAsset("lambda"),
             handler: "hello.handler",
+        });
+
+        new apigw.LambdaRestApi(this, "Endpoint", {
+            handler: hello,
         });
     }
 }
