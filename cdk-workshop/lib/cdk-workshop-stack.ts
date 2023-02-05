ef4bdbda0a2ce6b67889e4e207437d3b4c0b6892
JordanGottardo <jordangottardo@gmail.com>
2023-02-05 17:22:55 +0100

    introduce cdk watch

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,9 +1,16 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 import { Construct } from "constructs";
 
 export class CdkWorkshopStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const hello = new lambda.Function(this, "HelloHandler", {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            code: lambda.Code.fromAsset("lambda"),
+            handler: "hello.handler",
+        });
     }
 }
