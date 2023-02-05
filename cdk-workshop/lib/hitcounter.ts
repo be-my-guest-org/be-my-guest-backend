8289be679dd1333eb78660c7460347726e2585a2
JordanGottardo <jordangottardo@gmail.com>
2023-02-05 18:14:36 +0100

    define dynamoDb table and hitcounter lambda

--- cdk-workshop/lib/hitcounter.ts
+++ cdk-workshop/lib/hitcounter.ts
@@ -1,3 +1,4 @@
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
@@ -7,7 +8,24 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
+    /** allows accesing the counter function */
+    public readonly handler: lambda.Function;
+
     constructor(scope: Construct, id: string, props: HitCounterProps) {
         super(scope, id);
+
+        const table = new dynamodb.Table(this, "Hits", {
+            partitionKey: { name: "path", type: dynamodb.AttributeType.STRING },
+        });
+
+        this.handler = new lambda.Function(this, "HitCounterHandler", {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: "hitcounter.handler",
+            code: lambda.Code.fromAsset("lambda"),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName,
+            },
+        });
     }
 }
