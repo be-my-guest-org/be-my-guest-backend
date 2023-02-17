2a72d841c1c0c52a889bd4947c603aba807ff8e4
JordanGottardo <jordangottardo@gmail.com>
2023-02-17 19:29:06 +0100

    separate api to different file

--- infra/lib/be-my-guest-stack.ts
+++ infra/lib/be-my-guest-stack.ts
@@ -1,10 +1,3 @@
-import {
-  CorsHttpMethod,
-  HttpApi,
-  HttpMethod,
-} from "@aws-cdk/aws-apigatewayv2-alpha";
-import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
-import { DotNetFunction } from "@xaaskit-cdk/aws-lambda-dotnet";
 import * as cdk from "aws-cdk-lib";
 import { RemovalPolicy, SecretValue } from "aws-cdk-lib";
 import {
@@ -16,70 +9,14 @@ import {
   VerificationEmailStyle,
 } from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
+import { Api } from "./constructs/Api";
 
 export class BeMyGuestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const beMyGuestLambda = new DotNetFunction(this, "BeMyGuest", {
-      projectDir: "../BeMyGuest/BeMyGuest.Api/src/BeMyGuest.Api",
-    });
-
-    const httpApi = new HttpApi(this, "be-my-guest", {
-      description: "Be my guest API gateway",
-      corsPreflight: {
-        allowHeaders: [
-          "Content-Type",
-          "X-Amz-Date",
-          "Authorization",
-          "X-Api-Key",
-        ],
-        allowMethods: [
-          CorsHttpMethod.OPTIONS,
-          CorsHttpMethod.GET,
-          CorsHttpMethod.POST,
-          CorsHttpMethod.PUT,
-          CorsHttpMethod.PATCH,
-          CorsHttpMethod.DELETE,
-        ],
-        allowCredentials: true,
-        allowOrigins: ["http://localhost:3000"],
-      },
-    });
-
-    const integration = new HttpLambdaIntegration(
-      "integration",
-      beMyGuestLambda
-    );
-
-    httpApi.addRoutes({
-      path: "/",
-      methods: [HttpMethod.GET],
-      integration: integration,
-    });
-
-    httpApi.addRoutes({
-      path: "/calculator/add/{a}/{b}",
-      methods: [HttpMethod.GET],
-      integration: integration,
-    });
-
-    httpApi.addRoutes({
-      path: "/calculator/echo",
-      methods: [HttpMethod.GET],
-      integration: integration,
-    });
-
-    httpApi.addRoutes({
-      path: "/calculator/echo2/{x}",
-      methods: [HttpMethod.GET],
-      integration: integration,
-    });
-
-    new cdk.CfnOutput(this, "apiUrl", {
-      value: httpApi.url!,
-    });
-
+    new Api(this, "Api");
+    
     const secrets = new cdk.aws_secretsmanager.Secret(
       this,
       "be-my-guest-secrets",
