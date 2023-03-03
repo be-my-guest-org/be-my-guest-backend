4ec299ac06c2a8503eb98357c747bf299cbd8046
JordanGottardo <jordangottardo@gmail.com>
2023-03-03 16:43:04 +0100

    setup clean architecture

--- BeMyGuest/BeMyGuest.Api/src/BeMyGuest.Api/Program.cs
+++ BeMyGuest/BeMyGuest.Api/src/BeMyGuest.Api/Program.cs
@@ -1,3 +1,6 @@
+using BeMyGuest.Application;
+using BeMyGuest.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
 // package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
---
+++ BeMyGuest/BeMyGuest.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeMyGuest.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var markerType = typeof(DependencyInjection);
+
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssemblyContaining(markerType);
+        });
+
+        services.AddValidatorsFromAssemblyContaining(markerType);
+
+        return services;
+    }
+}
