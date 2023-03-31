91901bf51c5d14cc8a3eed1123a2965c5b61113c
Jordan <jordangottardo@gmail.com>
2023-03-31 18:00:06 +0200

    add event status (#39)

---
+++ BeMyGuest/BeMyGuest.Domain/Events/ValueObjects/Status.cs
@@ -0,0 +1,29 @@
+﻿namespace BeMyGuest.Domain.Events.ValueObjects;
+
+public class Status
+{
+    private const string OpenValue = "Open";
+    private const string ClosedValue = "Closed";
+    private const string FullValue = "Full";
+    private const string ExpiredValue = "Expired";
+
+    private Status(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    public static Status Open() => new Status(OpenValue);
+
+    public static Status Closed() => new Status(ClosedValue);
+
+    public static Status Full() => new Status(FullValue);
+
+    public static Status Expired() => new Status(ExpiredValue);
+
+    public static Status From(string status)
+    {
+        return new Status(status);
+    }
+}
