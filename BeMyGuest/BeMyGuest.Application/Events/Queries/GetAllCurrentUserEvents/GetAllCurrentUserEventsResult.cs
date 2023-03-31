38259db032c4b9d6c3192cefc2bdf79baf99c827
Jordan <jordangottardo@gmail.com>
2023-03-31 17:30:15 +0200

    get all events for user (#38)

---
+++ BeMyGuest/BeMyGuest.Application/Events/Queries/GetAllCurrentUserEvents/GetAllCurrentUserEventsResult.cs
@@ -0,0 +1,9 @@
+﻿using BeMyGuest.Domain.Events;
+using OneOf;
+
+namespace BeMyGuest.Application.Events.Queries.GetAllCurrentUserEvents;
+
+[GenerateOneOf]
+public partial class GetAllCurrentUserEventsResult : OneOfBase<List<Event>>
+{
+}
